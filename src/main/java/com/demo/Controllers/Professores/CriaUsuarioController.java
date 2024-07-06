e312ef0ff28530e67190198fa52f2605221b9000
jorge <jorgecsn@id.uff.br>
2024-07-06 16:31:41 -0300

    add validacoes ao criar usuario

--- src/main/java/com/demo/Controllers/Professores/CriaUsuarioController.java
+++ src/main/java/com/demo/Controllers/Professores/CriaUsuarioController.java
@@ -4,6 +4,7 @@ import com.demo.Classes.Busca;
 import com.demo.Classes.Classe;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
@@ -25,17 +26,25 @@ public class CriaUsuarioController {
   public Button criar;
   public Button voltar;
   public Text mensagemresposta;
+  public ChoiceBox<String> nivel;
 
   public void initialize() {
+    nivel.getItems().addAll("1", "2", "3", "4");
     criar.setOnAction(event -> salvaUsuario());
     voltar.setOnAction(event -> voltarTela());
   }
 
   private void salvaUsuario() {
-    if (uemail.getText().isEmpty() || senha.getText().isEmpty() || nome.getText().isEmpty() || tipo.getText().isEmpty()) {
-      mensagemresposta.setFill(Color.RED);
-      mensagemresposta.setText("Todos os campos devem ser preenchidos");
-      return;
+    if (tipo.getText().equals("a") && (uemail.getText().isEmpty() || nivel.getValue() == null || senha.getText().isEmpty() || nome.getText().isEmpty() || tipo.getText().isEmpty())) {
+        mensagemresposta.setFill(Color.RED);
+        mensagemresposta.setText("Todos os campos devem ser preenchidos");
+        return;
+    }
+
+    if (tipo.getText().equals("p") && (uemail.getText().isEmpty() || senha.getText().isEmpty() || nome.getText().isEmpty() || tipo.getText().isEmpty())) {
+        mensagemresposta.setFill(Color.RED);
+        mensagemresposta.setText("Todos os campos devem ser preenchidos");
+        return;
     }
 
     if (!tipo.getText().equals("a") && !tipo.getText().equals("p")) {
@@ -54,15 +63,29 @@ public class CriaUsuarioController {
          BufferedWriter bw = new BufferedWriter(fw);
          PrintWriter out = new PrintWriter(bw)) {
 
-      String dados = String.format("%s,%s,%s,1,%s\n",
-              uemail.getText().toLowerCase(),
-              senha.getText(),
-              nome.getText(),
-              tipo.getText());
-      out.print(dados);
-      mensagemresposta.setFill(Color.BLACK);
-      mensagemresposta.setText("Usuário cadastrado com sucesso");
-      limparCampos();
+      if(tipo.getText().equals("a")){
+        String dados = String.format("%s,%s,%s,%s,%s\n",
+                uemail.getText().toLowerCase(),
+                senha.getText(),
+                nome.getText(),
+                nivel.getValue(),
+                tipo.getText());
+        out.print(dados);
+        mensagemresposta.setFill(Color.BLACK);
+        mensagemresposta.setText("Usuário cadastrado com sucesso");
+        limparCampos();
+      }
+      else if(tipo.getText().equals("p")) {
+        String dados = String.format("%s,%s,%s,%s\n",
+                uemail.getText().toLowerCase(),
+                senha.getText(),
+                nome.getText(),
+                tipo.getText());
+        out.print(dados);
+        mensagemresposta.setFill(Color.BLACK);
+        mensagemresposta.setText("Usuário cadastrado com sucesso");
+        limparCampos();
+      }
 
     } catch (IOException e) {
       mensagemresposta.setFill(Color.RED);
