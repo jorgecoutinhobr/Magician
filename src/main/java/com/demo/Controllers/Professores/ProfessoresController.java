cb1586286592a16ff69cb53b5f2b7fd96b022078
jorge <jorgecsn@id.uff.br>
2024-07-06 12:07:17 -0300

    add sair button

--- src/main/java/com/demo/Controllers/Professores/ProfessoresController.java
+++ src/main/java/com/demo/Controllers/Professores/ProfessoresController.java
@@ -2,12 +2,17 @@ package com.demo.Controllers.Professores;
 
 import com.demo.Classes.Busca;
 import com.demo.Classes.Classe;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.awt.*;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class ProfessoresController {
@@ -15,10 +20,12 @@ public class ProfessoresController {
   public Text bv;
   public Button criapergunta;
   public Button criausuario;
+  public Button btnSair;
 
   public void initialize(String email) {
     criapergunta.setOnAction(event -> criadorPergunta());
     criausuario.setOnAction(event -> criadorUsuario());
+    btnSair.setOnAction(event -> retornarLogin());
     ArrayList<String> array = Busca.usuario(email);
     int index = array.get(2).indexOf(" ");
     String nome;
@@ -41,4 +48,11 @@ public class ProfessoresController {
     currentStage.close();
     Classe.getInstance().getView().showCriaUsuarioWindow();
   }
+
+  private void retornarLogin() {
+    Stage currentStage = (Stage) btnSair.getScene().getWindow();
+    currentStage.close();
+    Classe.getInstance().getView().showLoginWindow();
+  }
+
 }
