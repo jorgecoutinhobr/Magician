60f91547563d9674e79442bc5d5c16b959bccec2
Gabriel Villa Verde dos Reis <gab.villaverde@gmail.com>
2024-07-06 22:23:21 -0300

    a

--- src/main/java/com/demo/Classes/Usuario.java
+++ src/main/java/com/demo/Classes/Usuario.java
@@ -2,46 +2,6 @@ package com.demo.Classes;
 import java.util.ArrayList;
 
 public class Usuario {
-  private String email;
-  private String senha;
-  private String tipo;
-  private String nome;
-
-  public Usuario(String email, String senha, String tipo){
-    this.email = email;
-    this.senha = senha;
-    this.tipo = tipo;
-  }
-
-  public String getEmail(){
-    return email;
-  }
-
-
-  public String getSenha(){
-    return senha;
-  }
-
-  public String getTipo(){
-    return tipo;
-  }
-  public String getNome(){return nome; }
-
-  public void setEmail(String email) {
-    this.email = email;
-  }
-
-  public void setSenha(String senha) {
-    this.senha = senha;
-  }
-
-  public void setTipo(String tipo) {
-    this.tipo = tipo;
-  }
-
-  public void setNome(String nome) {
-    this.nome = nome;
-  }
 
   public static String[] autenticar(String email, String senha){
     ArrayList<String> dadosU = Busca.usuario(email);
